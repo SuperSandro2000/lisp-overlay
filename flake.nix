61c3871361bc4d48d3035a24be229b8b6957f6c8
Tony Olagbaiye <me@fron.io>
2020-12-07 18:43:49 +0000

    quicklisp: add repos.quicklisp.distinfos

--- flake.nix
+++ flake.nix
@@ -4,87 +4,48 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs = inputs@{ self, nixpkgs }: let
+
     forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+
+    urls.quicklisp = {
+      latest = "http://beta.quicklisp.org/dist/quicklisp.txt";
+      versions = "http://beta.quicklisp.org/dist/quicklisp-versions.txt";
+    };
+
   in {
 
     repos = {
       quicklisp = rec {
-        # ohoho use  http://beta.quicklisp.org/dist/quicklisp.txt  and
-        #            http://beta.quicklisp.org/dist/quicklisp-versions.txt  ?
+        subscription = if builtins.all builtins.pathExists [
+          ./quicklisp/dist/quicklisp.txt
+          ./quicklisp/dist/quicklisp-versions.txt
+        ] then {
+          latest = builtins.readFile ./quicklisp/dist/quicklisp.txt;
+          versions = builtins.readFile ./quicklisp/dist/quicklisp-versions.txt;
+        } else builtins.throw "Run .#quicklisp-update-subscription first";
 
-        latest = let inherit (nixpkgs) lib; in builtins.listToAttrs (
+        latest = builtins.listToAttrs (
           builtins.map
-            (s: let kv = lib.splitString ": " s; in { name = lib.head kv; value = lib.last kv; })
-            (lib.init (lib.splitString "\n" (builtins.readFile (builtins.fetchurl { url = "http://beta.quicklisp.org/dist/quicklisp.txt"; }))))
+            (s: let kv = nixpkgs.lib.splitString ": " s; in {
+              name = nixpkgs.lib.head kv;
+              value = nixpkgs.lib.last kv;
+            })
+            (nixpkgs.lib.init (nixpkgs.lib.splitString "\n" subscription.latest))
         );
 
-        # Quicklisp dist attrset
-        dists = let
-          filldist = value@{ name, ... }: (nixpkgs.lib.genAttrs
-            ["systems" "releases" "distinfo"] (file:
-              "http://beta.quicklisp.org/dist/quicklisp/${name}/${file}.txt"
-            )
-          ) // value;
-          distlist = builtins.map (entry: let
-            value = filldist entry;
-          in {
-            inherit (value) name;
-            inherit value;
-          }) (builtins.fromJSON (builtins.readFile ./quicklisp/dists.json));
-        in builtins.listToAttrs distlist // {
-          latest = (builtins.head distlist).value;
-        };
-
-        # Filename to url mapping
-        archives = let
-          filespecs = builtins.concatLists (builtins.attrValues dists);
-        in builtins.listToAttrs (builtins.map ({ name, url, ... }: {
-          name = name;
-          value = url;
-        }) filespecs);
-
-        # Repository dist pin
-        meta = builtins.fromJSON (builtins.readFile ./quicklisp/meta.json);
-
-        # Quicklisp dist fetch
-        dist = forAllSystems (system: let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in pkgs.fetchgit {
-          inherit (self.repos.quicklisp.meta) url rev sha256;
-        });
-
-        # Package specs
-        specs = forAllSystems (system: builtins.mapAttrs
-          (name: _: let
-            value = nixpkgs.lib.removeSuffix "\n" (
-              builtins.readFile "${dist.${system}}/projects/${name}/source.txt"
-            );
-            kvPair = nixpkgs.lib.splitString " " value;
-          in {
-            type = nixpkgs.lib.elemAt kvPair 0;
-            url = nixpkgs.lib.elemAt kvPair 1;
-            data = if builtins.length kvPair > 2
-                   then nixpkgs.lib.elemAt kvPair 2
-                   else null;
-            inherit value;
-          })
-          (builtins.readDir "${dist.${system}}/projects"));
-
-        # Package updater script snippets
-        updaters = forAllSystems (system: let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in builtins.mapAttrs
-          (name: { type, url, ... }: let
-            inherit (self.lib.quicklisp) handler;
-          in if builtins.hasAttr type handler then handler.${type} { inherit name url pkgs; }
-             else builtins.trace "Quicklisp - Unhandled type: ${type} (for ${name})" null)
-          (specs.${system})
+        versions = builtins.listToAttrs (
+          builtins.map
+            (s: let kv = nixpkgs.lib.splitString " " s; in {
+              name = nixpkgs.lib.head kv;
+              value = nixpkgs.lib.last kv;
+            })
+            (nixpkgs.lib.init (nixpkgs.lib.splitString "\n" subscription.versions))
         );
 
-        # Enumeration of valid "type" values, just to eval
-        types = forAllSystems (system: nixpkgs.lib.unique (
-          nixpkgs.lib.mapAttrsToList (k: v: v.type) specs.${system}
-        ));
+        distinfos = nixpkgs.lib.filterAttrs (dist: builtins.pathExists) (
+          builtins.mapAttrs
+            (dist: _: ./quicklisp/dist + "/${dist}/distinfo.txt")
+            (builtins.readDir ./quicklisp/dist));
       };
     };
 
@@ -101,75 +62,62 @@
     in {
       quicklisp-update-subscription = rec {
         type = "app";
-        urls.latest = "http://beta.quicklisp.org/dist/quicklisp.txt";
-        urls.versions = "http://beta.quicklisp.org/dist/quicklisp-versions.txt";
-        program = (pkgs.writeShellScript "update-quicklisp-dist" ''set -ex
+        message = "quicklisp: update subscription";
+        program = (pkgs.writeShellScript "update-quicklisp-subscription" ''set -ex
           test -e flake.nix || (echo "Must be run from repo root"; exit 1)
           ${pkgs.coreutils}/bin/mkdir -p quicklisp/dist
 
-          ${pkgs.curl}/bin/curl -L '${urls.latest}' > quicklisp/dist/quicklisp.txt
-          ${pkgs.curl}/bin/curl -L '${urls.versions}' > quicklisp/dist/quicklisp-versions.txt
+          ${pkgs.curl}/bin/curl -L '${urls.quicklisp.latest}' > quicklisp/dist/quicklisp.txt
+          ${pkgs.curl}/bin/curl -L '${urls.quicklisp.versions}' > quicklisp/dist/quicklisp-versions.txt
+          ${pkgs.git}/bin/git add \
+            quicklisp/dist/quicklisp.txt quicklisp/dist/quicklisp-versions.txt \
+          && ${pkgs.git}/bin/git commit -m '${message}' -- \
+            quicklisp/dist/quicklisp.txt quicklisp/dist/quicklisp-versions.txt \
+            || true
+
+          echo Done.
+        '').outPath;
+      };
+
+      quicklisp-update-distinfos = rec {
+        type = "app";
+        program = (pkgs.writeShellScript "update-quicklisp-distinfos" ''set -ex
+          test -e flake.nix || (echo "Must be run from repo root"; exit 1)
+
+          ${nixpkgs.lib.concatMapStringsSep "" (dist: let
+            url = self.repos.quicklisp.versions.${dist};
+            message = "quicklisp: update distinfo for ${dist}";
+          in ''
+            ${pkgs.coreutils}/bin/mkdir -p quicklisp/dist/${dist}
+            ${pkgs.curl}/bin/curl -L '${url}' > quicklisp/dist/${dist}/distinfo.txt
+            ${pkgs.git}/bin/git add quicklisp/dist/${dist}/distinfo.txt && \
+            ${pkgs.git}/bin/git commit -m '${message}' -- \
+              quicklisp/dist/${dist}/distinfo.txt \
+              || true
+          '') (builtins.attrNames self.repos.quicklisp.versions)}
+
+          echo Done.
         '').outPath;
       };
 
       quicklisp-update-dists = rec {
         type = "app";
-        url = "https://api.github.com/repos/roswell/quicklisp/releases?page=";
-        program = (pkgs.writeShellScript "update-quicklisp-dist" ''set -ex
+        program = (pkgs.writeShellScript "update-quicklisp-dists" ''set -ex
           test -e flake.nix || (echo "Must be run from repo root"; exit 1)
-          ${pkgs.coreutils}/bin/mkdir -p quicklisp
 
-          while
-            ${pkgs.curl}/bin/curl -L "${url}''${page:-1}" \
-              | ${pkgs.jq}/bin/jq -e 'select(length != 0)' \
-              > quicklisp/.dists.''${page:-1}.json
-          do
-            page=$((''${page:-1}+1))
-          done
+          ${nixpkgs.lib.concatMapStringsSep "" (dist: let
+            url = self.repos.quicklisp.versions.${dist};
+            message = "quicklisp: update distinfo for ${dist}";
+          in ''
+            ${pkgs.coreutils}/bin/mkdir -p quicklisp/dist/${dist}
+            ${pkgs.curl}/bin/curl -L '${url}' > quicklisp/dist/${dist}/distinfo.txt
+            ${pkgs.git}/bin/git add quicklisp/dist/${dist}/distinfo.txt && \
+            ${pkgs.git}/bin/git commit -m '${message}' -- \
+              quicklisp/dist/${dist}/distinfo.txt \
+              || true
+          '') (builtins.attrNames self.repos.quicklisp.versions)}
 
-          ${pkgs.coreutils}/bin/rm quicklisp/.dists.json
-          for ((p=1; p<$page; p++)); do
-            ${pkgs.coreutils}/bin/cat quicklisp/.dists.$p.json \
-              >> quicklisp/.dists.json
-          done
-
-          ${pkgs.jq}/bin/jq -s '[.[][]] | map({ name: .tag_name, files: .assets | map({ name: .name, id: .id, url: .browser_download_url }) })' quicklisp/.dists.json \
-            > quicklisp/dists.json
-        '').outPath;
-      };
-      quicklisp-update-dist = rec {
-        type = "app";
-        url = "git://github.com/quicklisp/quicklisp-projects";
-        program = (pkgs.writeShellScript "update-quicklisp-dist" ''set -ex
-          test -e flake.nix || (echo "Must be run from repo root"; exit 1)
-          mkdir -p quicklisp
-          ${pkgs.nix-prefetch-git}/bin/nix-prefetch-git --url '${url}' > quicklisp/meta.json
-        '').outPath;
-      };
-      quicklisp-update-pkgs = rec {
-        type = "app";
-        updaters = self.repos.quicklisp.updaters.${system};
-        program = (pkgs.writeShellScript "update-quicklisp-pkgs" ''set -ex
-          test -e flake.nix || (echo "Must be run from repo root"; exit 1)
-          mkdir -p quicklisp && cd quicklisp
-          ${concatStringsSep "" (mapAttrsToList (name: updater: ''
-            echo -- ${name}
-            ${if updater != null then updater else "echo Unavailable"}
-          '') updaters)}
-        '').outPath;
-      };
-      quicklisp-add-pkgs = rec {
-        type = "app";
-        updaters = self.repos.quicklisp.updaters.${system};
-        program = (pkgs.writeShellScript "update-quicklisp-pkgs" ''set -ex
-          test -e flake.nix || (echo "Must be run from repo root"; exit 1)
-          mkdir -p quicklisp && cd quicklisp
-          ${concatStringsSep "" (mapAttrsToList (name: updater: ''
-            echo -- ${name}
-            if test ! -e ${name}; then
-              ${if updater != null then updater else "echo Unavailable"}
-            fi
-          '') updaters)}
+          echo Done.
         '').outPath;
       };
     });
